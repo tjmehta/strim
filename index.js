0ddc06772b15af3edf5a485de6ac93bf9c14f2b3
Casey Flynn <cflynn.us@gmail.com>
2015-02-24 16:14:03 -0800

    save npm dep "blacklight", renaming files for consistency

--- index.js
+++ index.js
@@ -3,32 +3,31 @@
 var through = require('through');
 var defaults = require('101/defaults');
 
-/**
- * Create a strim - stream whitespace trimmer
- * @param  {object} opts
- * @param  {boolean} opts.removeLineTrailing - whether line-trailing whitespace
- * @param  {boolean} opts.removeBlankLines   - whether or not to remove blank lines
- * @return {DuplexStream} strim - stream whitespace trimmer
- */
+module.exports = createStrim;
 
 var streams = {
-  // TODO: removeLineLeading  : require('remove-line-leading')(),
-  trimLineTrailing : require('trim-line-trailing')(),
-  trimBlankLines   : require('trim-blank-lines')()
+  removeLineLeading:  require('./lib/remove-line-leading')(),
+  removeLineTrailing: require('./lib/remove-line-trailing')(),
+  removeBlankLines:   require('./lib/remove-blank-lines')()
 };
 
-module.exports = function createStrim (opts) {
-  opts = defaults(opts)
+/**
+ * Create a strim - stream whitespace trimmer
+ * @param {object} opts
+ * @return {DuplexStream} strim - stream whitespace trimmer
+ */
+function createStrim (opts) {
+  opts = defaults(opts);
   return [
-    // TODO: 'removeLineLeading',
-    'trimLineTrailing',
-    'trimBlankLines'
+    'removeLineLeading',
+    'removeLineTrailing',
+    'removeBlankLines'
   ].reduce(function (stream, transform) {
     return opts[transform] ?
       stream.pipe(streams[transform]) :
       stream;
   }, through(identity));
-};
+}
 
 function identity (data) {
   this.queue(data);
---
+++ test/remove-line-leading.js
@@ -0,0 +1,97 @@
+'use strict';
+var Code = require('code');
+var Lab = require('lab');
+var lab = exports.lab = Lab.script();
+
+var describe = lab.describe;
+var it = lab.it;
+var beforeEach = lab.beforeEach;
+var expect = Code.expect;
+var concat = require('concat-stream');
+var blacklight = require('blacklight');
+var trailWhitespace = /^(.*[^\s]+)(\s+)$/;
+
+describe('remove line leading whitespace', function () {
+  var ctx;
+  describe('blank lines', function () {
+    beforeEach(function (done) {
+      ctx = {};
+      var data = ' \n\t\n\r\n \t\r\n';
+      ctx.chunks = data.split('');
+      ctx.expected = replaceLineTrailingWhitespaceInString(data);
+      done();
+    });
+    describe('strings', function () {
+      it('should trim blank lines', shouldTrimLines);
+    });
+    describe('buffers', function () {
+      beforeEach(function (done) {
+        ctx.chunks = ctx.chunks.map(newBuffer);
+        done();
+      });
+      it('should trim blank lines', shouldTrimLines);
+    });
+  });
+  describe('trailing whitespace lines', function () {
+    beforeEach(function (done) {
+      ctx = {};
+      var data = [
+        ' \n',
+        '\t\n',
+        '\r\n',
+        ' \t\r\n',
+        ' \n',
+        'hey\t\n',
+        'hey\r\n',
+        'hey \t\r\n',
+        '\they\n',
+        '\rhey\n',
+        ' \they'
+      ];
+      ctx.chunks = data;
+      ctx.expected = replaceLineTrailingWhitespaceInString(data.join(''));
+      done();
+    });
+    describe('strings', function () {
+      it('should trim trailing whitespace', shouldTrimLines);
+    });
+    describe('buffers', function () {
+      beforeEach(function (done) {
+        ctx.chunks = ctx.chunks.map(newBuffer);
+        done();
+      });
+      it('should trim trailing whitespace', shouldTrimLines);
+    });
+    describe('ends with whitespace after end line', function() {
+      beforeEach(function (done) {
+        ctx.chunks.push('\nhey ');
+        ctx.expected += '\nhey';
+        done();
+      });
+      describe('strings', function () {
+        it('should trim trailing whitespace', shouldTrimLines);
+      });
+    });
+  });
+  function shouldTrimLines (done) {
+    var replaceBlankLines = require('../lib/replace-line-trailing')();
+    replaceBlankLines.pipe(concat(function (data) {
+      expect(data.toString()).to.equal(ctx.expected);
+      done();
+    }));
+    ctx.chunks.forEach(function (chunk) {
+      replaceBlankLines.write(chunk);
+    });
+    replaceBlankLines.end();
+  }
+});
+
+
+function newBuffer (s) {
+  return new Buffer(s);
+}
+function replaceLineTrailingWhitespaceInString (str) {
+  return str.split('\n').map(function (line) {
+    return line.replace(trailWhitespace, '$1').replace(/^\s+$/, '');
+  }).join('\n');
+}
