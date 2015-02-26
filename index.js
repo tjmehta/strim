e96e599ffd48ce222717ab9ac36d166153fb3eab
Casey Flynn <cflynn.us@gmail.com>
2015-02-25 16:10:49 -0800

    remove lineleading trim

--- index.js
+++ index.js
@@ -19,7 +19,6 @@ var streams = {
 function createStrim (opts) {
   opts = defaults(opts);
   return [
-    'removeLineLeading',
     'removeLineTrailing',
     'removeBlankLines'
   ].reduce(function (stream, transform) {
