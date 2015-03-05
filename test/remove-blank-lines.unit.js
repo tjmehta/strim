62754dbb67086cad4e780f2102301bc9af492ffe
Casey Flynn <cflynn.us@gmail.com>
2015-03-05 16:36:06 -0500

    test fixtures, testing all 3 chunk combinations

--- test/remove-blank-lines.unit.js
+++ test/remove-blank-lines.unit.js
@@ -2,7 +2,9 @@
 
 var Code = require('code');
 var Lab = require('lab');
+var async = require('async');
 var concat = require('concat-stream');
+var through = require('through');
 
 var lab = exports.lab = Lab.script();
 
@@ -11,39 +13,49 @@ var describe = lab.describe;
 var expect = Code.expect;
 var it = lab.it;
 
+var equivalentDockerfiles = require('./fixtures/equivalent-dockerfiles');
+
 describe('remove blank lines', function () {
   var ctx;
-  beforeEach(function (done) {
-    ctx = {};
-    var data = ' \n\t\n\r\n \t\r\n';
-    ctx.chunks = data.split('');
-    ctx.expected = data.replace(/^[\s]*\n$/gm, '');
-    done();
-  });
-  describe('strings', function () {
-    it('should remove blank lines', shouldRemoveBlankLines);
-  });
-  describe('buffers', function () {
-    beforeEach(function (done) {
-      ctx.chunks = ctx.chunks.map(newBuffer);
-      done();
-    });
-    // failing test
-    it('should remove blank lines', shouldRemoveBlankLines);
-  });
-  function shouldRemoveBlankLines (done) {
-    var replaceBlankLines = require('../lib/remove-blank-lines')();
-    replaceBlankLines.pipe(concat(function (data) {
-      expect(data.toString()).to.equal(ctx.expected);
-      done();
-    }));
-    ctx.chunks.forEach(function (chunk) {
-      replaceBlankLines.write(chunk);
-    });
-    replaceBlankLines.end();
-  }
-});
 
-function newBuffer (s) {
-  return new Buffer(s);
-}
+  describe('all combinations of chunks', function () {
+
+    beforeEach(function (done) {
+      ctx = {};
+      var dockerfile = ctx.dockerfile = new Buffer(" \t\r\n \t\r\n \t\r\n \t\r\nFROM "+
+                                  "dockerfile/nodejs \t\r\n \t\r\n "+
+                                  "\t\r\n \t\r\nCMD tail -f /var/log/dpkg.log "+
+                                  "\t\r\n \t\r\n \t\r\n \t\r\n");
+      ctx.expected = "FROM dockerfile/nodejs \t\nCMD tail -f /var/log/dpkg.log \t";
+      var chunkSets = ctx.chunkSets =  [];
+      for (var i = 0; i+8 < dockerfile.length; i++) {
+        var chunks = chunkSets[i] = [];
+        if (i !== 0)  { chunks.push(dockerfile.slice(0, i)); }
+        chunks.push(dockerfile.slice(i, i+8));
+        chunks.push(dockerfile.slice(i+8, dockerfile.length));
+      }
+      done();
+    });
+
+    it('should correctly trim', function (done) {
+      async.each(ctx.chunkSets, function (chunks, cb) {
+        var replaceBlankLines = require('../lib/remove-blank-lines')();
+        var data = '';
+        replaceBlankLines.on('data', function (d) {
+          data += d;
+        });
+        chunks.forEach(function (chunk) {
+          replaceBlankLines.write(chunk);
+        });
+        replaceBlankLines.end();
+        expect(data).to.equal(ctx.expected);
+        cb();
+      }, done);
+    });
+  });
+
+  function toString (buffer) {
+    return buffer.toString();
+  }
+
+});
